deea6aad01a4036c3898ddc99084eb1b32e39da4
white1107 <sakisasaki0205@gmail.com>
2021-11-09 20:45:40 +0900

    3

--- 3/src/index.js
+++ 3/src/index.js
@@ -1,3 +1,20 @@
+const title1 = document.getElementById("title");
+console.log(title1);
+
+const title2 = document.querySelector("#title");
+console.log(title2);
+
+const containers = document.getElementsByClassName("container");
+console.log(containers);
+
+const container = document.querySelector(".container");
+console.log(container);
+
+const divEl = document.createElement("div");
+console.log(divEl,"hel");
+
+
+
 // 追加ボタン押下時に実行する関数
 const onClickAdd = () => {
   // テキストボックスのElementを取得
